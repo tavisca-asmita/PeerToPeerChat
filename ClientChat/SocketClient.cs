a81066a886051e28d5fc9ee9aa452cf8979ec89b
Asmita Sharma <assharma@affinionds.com>
2019-08-08 01:56:06 +0530

    Code

---
+++ ClientChat/SocketClient.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace ClientChat
+{
+    public class SocketClient
+    {
+        public static void Main(string[] args)
+        {
+            StartClient();
+        }
+
+
+        public static void StartClient()
+        {
+            byte[] bytes = new byte[1024];
+
+            try
+            {
+                IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddress = host.AddressList[0];
+
+                //IPAddress ipAddress = IPAddress.Parse("172.16.5.187");
+                IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 11000);
+
+                  
+                Socket sender = new Socket(ipAddress.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    sender.Connect(ipEndPoint);
+                    
+
+                    Console.WriteLine("Socket connected to {0}",
+                        sender.RemoteEndPoint.ToString());
+
+                   
+                    while (true)
+                    {
+                        string clientMessage = Console.ReadLine();
+                        byte[] message = Encoding.ASCII.GetBytes(clientMessage);
+                        int byteSent = sender.Send(message);
+                                                
+                        if (clientMessage.ToLowerInvariant().Equals("bye"))
+                            break;
+
+                        byte[] servermessage = new byte[1024];
+                        int receivedMessage = sender.Receive(servermessage);
+                        string serverMessage = Encoding.ASCII.GetString(servermessage, 0, receivedMessage);
+
+                        Console.WriteLine("Server : {0}", serverMessage);
+                    }
+                    
+
+                        
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.ReadKey();
+            }
+        }
+    }
+}
+    
+
+
---
+++ ClientChat/SocketListener.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientChat
+{
+    public class SocketListener
+    {
+
+        public static void Main(string[] args)
+        {
+            StartServer();
+        }
+        
+        public static void StartServer()
+        {
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = host.AddressList[0];
+
+            //IPAddress ipAddress = IPAddress.Parse("172.16.5.234");
+            
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+            
+
+            try
+            {
+
+
+                Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                listener.Bind(localEndPoint);
+
+                listener.Listen(5);
+
+                Console.WriteLine("Waiting for connection!!!!");
+                
+                Socket handler = listener.Accept();
+
+                string message = null;
+
+                byte[] data = null;
+
+                while (true)
+                {
+                    data = new byte[1024];
+                    int dataRecieved = handler.Receive(data);
+                    message = Encoding.ASCII.GetString(data, 0, dataRecieved);
+
+                    Console.WriteLine("Client : {0}", message);
+
+                    if (message.ToLowerInvariant().Equals("bye"))
+                        break;
+
+                    string serverMessage = Console.ReadLine();
+                    byte[] dataSent = Encoding.ASCII.GetBytes(serverMessage);
+                    handler.Send(dataSent);
+                }
+                
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\n Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
